83258c8e67219ab02dab2757ee2666eb299ce4d2
Claire Savage <clairejsavage@gmail.com>
2016-02-21 17:31:14 -0800

    added a functional User model: created User seed file as well.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+first_sibling = User.create(
+  f_name: "Jennifer",
+  l_name: "Peters",
+  email: "jennifer@email.com",
+  password: "abc123"
+)
+
+second_sibling = User.create(
+  f_name: "Sarah",
+  l_name: "Campbell",
+  email: "sarah@email.com",
+  password: "abc123"
+)
+
+third_sibling = User.create(
+  f_name: "Emily",
+  l_name: "Landman",
+  email: "emily@email.com",
+  password: "abc123"
+)
